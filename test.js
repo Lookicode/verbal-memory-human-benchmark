9bb5925c5d8501ae0964c6f5d43cb3f0e0dd7128
Lookicode <lukasjanecek13@gmail.com>
2024-07-07 18:50:59 +0800

    test no longer needed

--- test.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    fetch('data.txt')
-        .then(response => response.text())
-        .then(text => {
-            document.getElementById('word').innerText = text;
-        })
-        .catch(error => console.error('Error fetching the text file:', error));
-});
