7db2485216abf6549b247fcdf95b2271a34e9a7c
Lookicode <lukasjanecek13@gmail.com>
2024-07-07 19:39:23 +0800

    start button, reset game after game over

--- app.js
+++ app.js
@@ -9,24 +9,20 @@ let allWords = [];
 
 let BIAS_TOWARDS_SEEN_WORDS = 0.4;
 
-document.addEventListener("DOMContentLoaded", async () => {
-  await fetch("dictionary.txt")
-    .then((response) => response.text())
-    .then((text) => text.split("\r\n"))
-    .then((splitLines) => {
-      splitLines.forEach((word) => {
-        allWords.push(word);
-      });
-    })
-    .catch((error) => console.error("Error fetching the text file:", error));
-
-  displayRandomWord(allWords);
+start.addEventListener("click", () => {
+  start.style.display = "none";
+  document.getElementById("word").innerText = "";
+  setTimeout(startGame, 500);
 });
+// document.addEventListener("DOMContentLoaded", async () => {
+
+// });
 
 newWord.addEventListener("click", () => {
   let evaluatedWord = document.getElementById("word").innerText;
   if (seenWords.includes(evaluatedWord)) {
     alert("Game over! Your score is: " + score);
+    resetGame();
     return;
   }
   currentWord = displayRandomWord(allWords);
@@ -39,6 +35,7 @@ seenWord.addEventListener("click", () => {
   let evaluatedWord = document.getElementById("word").innerText;
   if (!seenWords.includes(evaluatedWord)) {
     alert("Game over! Your score is: " + score);
+    resetGame();
     return;
   }
   currentWord = displayRandomWord(allWords);
@@ -58,4 +55,26 @@ function displayRandomWord(allWords) {
   let randomWord = allWords[Math.floor(Math.random() * allWords.length)];
   document.getElementById("word").innerText = randomWord;
   return randomWord;
+}
+
+async function startGame() {
+  await fetch("dictionary.txt")
+    .then((response) => response.text())
+    .then((text) => text.split("\r\n"))
+    .then((splitLines) => {
+      splitLines.forEach((word) => {
+        allWords.push(word);
+      });
+    })
+    .catch((error) => console.error("Error fetching the text file:", error));
+
+  displayRandomWord(allWords);
+}
+
+function resetGame() {
+  seenWords = [];
+  document.getElementById("word").innerText =
+    "You scored " + score + " point(s)!";
+  score = 0;
+  start.style.display = "inline";
 }
